2b15194b23a3208253261da9e321da2e60fd2d92
farteen <xujavacn@gmail.com>
2017-04-06 22:24:51 +0800

    Initial Commit

---
+++ MemoryProfilerTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryProfilerTesting
+//
+//  Created by Glasses on 06/04/2017.
+//  Copyright © 2017 Glasses. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryProfilerTesting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MemoryProfilerTesting
+//
+//  Created by Glasses on 06/04/2017.
+//  Copyright © 2017 Glasses. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
